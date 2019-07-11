22ae2067112f32187a266e1c5b3b179777ef1037
thefonso <thefonso@thefonso.com>
2019-07-10 21:38:32 -0400

    WIP: building up basic html and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './images/logo.svg';
+import logo from './images/qubit-logo.svg';
+import bell from './images/icon-bell.svg';
+import pencil from './images/icon-pencil.svg';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App container">
+      <nav className="navbar navbar-light white">
+        <a className="navbar-brand" href="#">
+          <img src={logo} className="Qubit-logo" alt="logo" />
         </a>
-      </header>
+        <img src={bell} alt="bell" className="bell"/>
+        <div className={"initials"}><span>MS</span></div> Mark Smith
+        <div className="union-fashion">
+          <InputGroup className={"union-fashion"}>
+             <FormControl
+                 placeholder="Union Fashion"
+                 aria-label="Username"
+                 aria-describedby="basic-addon1"
+             />
+          </InputGroup>
+          {/*<Button>Union Fashion</Button>*/}
+        </div>
+      </nav>
+
+      <main className={"main-section d-flex flex-row"}>
+        <aside className={"App-menu deepBlue"}>
+          <ul>
+            <li>Overview</li>
+            <li>Experiments</li>
+            <li>Segments</li>
+            <li>Insights</li>
+            <li>Visitor Cloud</li>
+          </ul>
+        </aside>
+        <section className={"content-container"}>
+          <header className="App-header d-flex flex-row green">
+            <div className={"header-title d-flex flex-column"}>
+              <InputGroup>
+               <FormControl
+                   placeholder="Mobile users who bought shoes on Black Friday"
+                   aria-label="header-title"
+                   aria-describedby="basic-addon2"
+               />
+              </InputGroup>
+              <div className={"caption"}>Created by Mark Smith</div>
+            </div>
+            <a className="icon" href="#">
+              <img src={pencil} className="icon-pencil" alt="edit title" />
+            </a>
+            <div className={"header-title-buttons d-flex justify-content-between"}>
+              <Button variant={"outline-secondary"}>Exit</Button>
+              <Button variant={"outline-secondary"}>Preview</Button>
+              <Button variant={"outline-secondary"} className={"hover-on"}>Save changes</Button>
+            </div>
+          </header>
+          <section className={"App-content bodyPanelGray"}>
+            <div><span>body content go here</span></div>
+          </section>
+        </section>
+      </main>
+
     </div>
   );
 }
