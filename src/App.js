de15afe1ce9b96b18d089d294ea1e1793d2bea6f
thefonso <thefonso@thefonso.com>
2019-07-11 08:36:53 -0400

    WIP: menu items alignment and hover colors

--- src/App.js
+++ src/App.js
@@ -40,11 +40,11 @@ function App() {
       <main className={"main-section d-flex flex-row"}>
         <aside className={"App-menu deepBlue"}>
           <ul>
-            <li className={"network"}>Overview</li>
-            <li className={"computers"}>Experiments</li>
-            <li className={"group"}>Segments</li>
-            <li className={"chart"}>Insights</li>
-            <li className={"cloud"}>Visitor Cloud</li>
+            <li className={"network"}><a href="overview.html">Overview</a> </li>
+            <li className={"computers"}><a href="experiments.html">Experiments</a> </li>
+            <li className={"group"}><a href="segments.html">Segments</a></li>
+            <li className={"chart"}><a href="insights.html">Insights</a></li>
+            <li className={"cloud"}><a href="visitor.html">Visitor Cloud</a></li>
           </ul>
         </aside>
         <section className={"content-container"}>
